8418c76cb32e4c67d447ad38ea18063eda9dd40a
Andrea Benetton <andrea.benetton@blueteam.ee>
2026-04-28 16:57:27 +0000

    Phase 7a: migrate Test.TrueCraft.API from NUnit to xUnit

--- Test.TrueCraft.API/TestBoundingCylinder.cs
+++ Test.TrueCraft.API/TestBoundingCylinder.cs
@@ -1,27 +1,26 @@
-﻿using NUnit.Framework;
 using TrueCraft.API;
+using Xunit;
 
 namespace Test.TrueCraft.API
 {
-    [TestFixture]
     public class TestBoundingCylinder
     {
-        [Test]
+        [Fact]
         public void TestIntersectsPoint()
         {
             //   x
             //  /
             // x
             var cylinder = new BoundingCylinder(Vector3.Zero, Vector3.One, 1);
-            Assert.IsTrue(cylinder.Intersects(cylinder.Min));
-            Assert.IsTrue(cylinder.Intersects(cylinder.Max));
-            Assert.IsTrue(cylinder.Intersects(cylinder.Min + (Vector3.One / 2)));
-            Assert.IsTrue(cylinder.Intersects(cylinder.Max - (Vector3.One / 2)));
-            Assert.IsTrue(cylinder.Intersects(new Vector3(0.25, 0, 0)));
-            Assert.IsFalse(cylinder.Intersects(new Vector3(5, 5, 5)));
+            Assert.True(cylinder.Intersects(cylinder.Min));
+            Assert.True(cylinder.Intersects(cylinder.Max));
+            Assert.True(cylinder.Intersects(cylinder.Min + (Vector3.One / 2)));
+            Assert.True(cylinder.Intersects(cylinder.Max - (Vector3.One / 2)));
+            Assert.True(cylinder.Intersects(new Vector3(0.25, 0, 0)));
+            Assert.False(cylinder.Intersects(new Vector3(5, 5, 5)));
         }
-        
-        [Test]
+
+        [Fact]
         public void TestIntersectsBox()
         {
             //   x
@@ -29,10 +28,9 @@ namespace Test.TrueCraft.API
             // x
             var cylinder = new BoundingCylinder(Vector3.Zero, Vector3.One * 10, 3);
             var doesNotIntersect = new BoundingBox(Vector3.One * 10 + 5, Vector3.One * 10 + 5);
-            Assert.IsFalse(cylinder.Intersects(doesNotIntersect));
+            Assert.False(cylinder.Intersects(doesNotIntersect));
             var intersects = new BoundingBox(Vector3.Zero, Vector3.One);
-            Assert.IsTrue(cylinder.Intersects(intersects));
+            Assert.True(cylinder.Intersects(intersects));
         }
     }
 }
-

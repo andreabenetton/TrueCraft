da47e952821eff4890728e099072633fe7b27a87
Andrea Benetton <andrea.benetton@blueteam.ee>
2026-05-16 14:07:23 +0200

    Iguina port: flesh out MainMenuView
    Restore navigation buttons: Singleplayer, Multiplayer, Options
    followed by Sign out (clears user + returns to LoginView) and Quit.
    All buttons are AutoCenter-anchored to match the prior layout.

--- TrueCraft.Launcher/Views/MainMenuView.cs
+++ TrueCraft.Launcher/Views/MainMenuView.cs
@@ -1,3 +1,4 @@
+using Iguina.Defs;
 using Iguina.Entities;
 
 namespace TrueCraft.Launcher.Views;
@@ -15,7 +16,34 @@ public sealed class MainMenuView : ILauncherView
     {
         parent.AddChild(new Title(_game.UI, $"Welcome, {_game.User.Username}"));
         parent.AddChild(new HorizontalLine(_game.UI));
-        parent.AddChild(new Paragraph(_game.UI, "TODO: migrate MainMenuView to Iguina"));
+        parent.AddChild(new RowsSpacer(_game.UI));
+
+        var singleplayer = new Button(_game.UI, "Singleplayer") { Anchor = Anchor.AutoCenter };
+        singleplayer.Events.OnClick = _ => _game.ShowView(new SingleplayerView(_game));
+        parent.AddChild(singleplayer);
+
+        var multiplayer = new Button(_game.UI, "Multiplayer") { Anchor = Anchor.AutoCenter };
+        multiplayer.Events.OnClick = _ => _game.ShowView(new MultiplayerView(_game));
+        parent.AddChild(multiplayer);
+
+        var options = new Button(_game.UI, "Options") { Anchor = Anchor.AutoCenter };
+        options.Events.OnClick = _ => _game.ShowView(new OptionView(_game));
+        parent.AddChild(options);
+
+        parent.AddChild(new RowsSpacer(_game.UI));
+
+        var signOut = new Button(_game.UI, "Sign out") { Anchor = Anchor.AutoCenter };
+        signOut.Events.OnClick = _ =>
+        {
+            _game.User.Username = null;
+            _game.User.SessionId = null;
+            _game.ShowView(new LoginView(_game));
+        };
+        parent.AddChild(signOut);
+
+        var quit = new Button(_game.UI, "Quit") { Anchor = Anchor.AutoCenter };
+        quit.Events.OnClick = _ => _game.Exit();
+        parent.AddChild(quit);
     }
 
     public void Dispose() { }
